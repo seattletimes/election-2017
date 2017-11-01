fb7b59b2ee644aad7f2cee3d07de3112f0380357
Thomas Wilburn <news@thomaswilburn.net>
2017-11-01 14:14:22 -0700

    Set up parties and featured races

--- generateConfig.js
+++ generateConfig.js
@@ -56,6 +56,29 @@ var countyList = [
   "Kitsap"
 ];
 
+// move these up to the top of the sheet
+var featured = [
+  41167,
+  41169,
+  115469,
+  41168,
+  361,
+  40998
+];
+
+var fixParty = function(p) {
+  var parties = {
+    "Republican": "R",
+    "Democrat": "D",
+    "Libertarian": "L",
+    "Independent": "I"
+  };
+  for (var k in parties) {
+    if (p.indexOf(k) >= 0) return parties[k];
+  }
+  return p;
+};
+
 var init = async function() {
   
   var stateRows = await parse(sos);
@@ -63,30 +86,34 @@ var init = async function() {
 
   var rows = stateRows.concat(countyRows);
 
+  // assemble unique race/candidate lists
   rows.forEach(function(r) {
-    var id = r.RaceID.trim();
+    var id = r.RaceID.trim() * 1;
     var type = r.RaceJurisdictionTypeName;
     var county = r.CountyName;
     if (county && countyList.indexOf(county) == -1) return;
+    var featureIndex = featured.indexOf(id) + 1;
     if (!races[id]) races[id] = {
       id,
       candidates: [],
       county: r.CountyName,
       name: titleCase(r.RaceName),
       category: categories[type] || type || "Local",
-      subcategory: r.CountyName
+      subcategory: r.CountyName,
+      featured: featureIndex || ""
     };
     races[id].candidates.push({
       race: id,
       id: r.BallotID,
       name: r.BallotName,
-      party: r.PartyName
+      party: fixParty(r.PartyName)
     });
   });
 
   var outputRaces = [];
   var outputCandidates = [];
 
+  // cull uncontested races
   for (var r in races) {
     var race = races[r];
     if (race.candidates.length <= 1) {
@@ -97,6 +124,8 @@ var init = async function() {
     outputRaces.push(race);
     outputCandidates.push(...race.candidates);
   }
+
+  outputRaces.sort((a, b) => (a.featured || Infinity) - (b.featured || Infinity));
 
   fs.writeFileSync("temp/races.csv", await serialize(outputRaces, raceColumns));
   fs.writeFileSync("temp/candidates.csv", await serialize(outputCandidates, candidateColumns));
